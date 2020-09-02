92e77cabc03110b941226d58d4e3d0e2b0401e9c
ajron23 <markocarko@yahoo.com>
2020-09-02 13:09:42 +0200

    Inicijalni commit

---
+++ MarkoSpringBootApp/src/main/java/com/sbs/MarkoSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.sbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkoSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkoSpringBootAppApplication.class, args);
+	}
+
+}
