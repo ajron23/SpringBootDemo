e1ffffccbd79f2b9f7623407c9f3fa70903d65f1
ajron23 <markocarko@yahoo.com>
2021-01-12 12:40:20 +0100

    Dodat prefix i sufix za jsp

--- MarkoSpringBootApp/src/main/java/com/sbs/controller/MarkoController.java
+++ MarkoSpringBootApp/src/main/java/com/sbs/controller/MarkoController.java
@@ -1,15 +1,16 @@
 package com.sbs.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
+@Controller
 public class MarkoController {
 	
 	@GetMapping("/")
 	public String home() {
-		//return "home.jsp";
-		return "Cao breeeeeeeee";
+		return "home";
+		//return "Cao breeeeeeeee";
 	}
 
 }
